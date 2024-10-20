10bdc55ebcefbca450820deec4a80e189c30e8a2
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-10-20 14:29:47 +0300

    feat: add temperature controller component to app component

---
+++ temperature-controller/src/App.tsx
@@ -0,0 +1,10 @@
+import { TemperatureController } from "./TemperatureController"
+import React from "react"
+
+function App() {
+  return (
+    <TemperatureController></TemperatureController>
+  )
+}
+
+export default App
